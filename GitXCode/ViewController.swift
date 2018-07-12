988f15d2e5095f92651d534a1aed8531f765b583
Alper Tellioglu <alpertellioglu@sabanciuniv.edu@onur-macbook-pro-2.local>
2018-07-12 20:26:18 +0300

    Initial Commit

---
+++ GitXCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXCode
+//
+//  Created by Alper Tellioglu on 12.07.2018.
+//  Copyright © 2018 Alpertt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
